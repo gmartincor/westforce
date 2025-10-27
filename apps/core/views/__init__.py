c13ea72e9faac2fbf76b90ea49379f8637f40214
gmartincor <guillermomc007@gmail.com>
2025-10-27 08:06:23 +0800

    authentication form

--- apps/core/views/__init__.py
+++ apps/core/views/__init__.py
@@ -1,4 +1,5 @@
 from .landing import landing_page
 from .health import health_check
+from .manager import manager_home
 
-__all__ = ['landing_page', 'health_check']
+__all__ = ['landing_page', 'health_check', 'manager_home']
